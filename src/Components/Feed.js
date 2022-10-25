afe2b08695a5d45add2aec6a6c03cd754e621b51
Anna Chung <achun@MacBook-Air-2.local>
2022-10-25 03:07:59 -0700

    alkdjsf;akfj

--- src/Components/Feed.js
+++ src/Components/Feed.js
@@ -4,18 +4,31 @@ import Menu from './Menu';
 import Block from './Block';
 
 const Feed = () => {
+    const [blocks, setBlocks] = useState([]); 
     /* TODO: Declare a new state variable to keep track of the blocks on your Blockstagram feed! */
     // Refer to Hint 2 for more help!
-
+    const updatePosts = (c)  => { 
+        var arr = [...blocks]; 
+        arr.unshift(c); 
+        setBlocks(arr); 
+        console.log(blocks);
+    }
     /* Use the map() function to render multiple Blocks! */
-    const posts = null; // TODO: edit this variable
+    // const posts = blocks.map( (eachColor) => 
+    //     <Block colour={eachColor}></Block> 
+    // ); // TODO: edit this variable
+    const posts = blocks.map( (eachColor) => 
+        <Block colour={eachColor}/>
+    ); 
+
 
     return (
         <div>
-            <Menu></Menu>
+            <Menu update={updatePosts}></Menu>
 
-            {/* Below is where all of your Blocks should render! */}
-            {posts}
+            {/*Below is where all of your Blocks should render!*/}
+            
+            <div>{posts}</div>
         </div>
     );
 }
--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import Color from './Color'; 
+
 /* Add any imports you think you might need here! */
 
-const Menu = () => { 
+const Menu = (props) => { 
 
     return (
       <div className="colorOptions">
-          {/* TODO */}
+        <div>
+          <Color handleClick = {props.update} color = "red" /> 
+        </div>
+        <div>
+          <Color handleClick = {props.update} color = "pink"/> 
+        </div>
+        <div>
+          <Color handleClick = {props.update} color = "blue"/> 
+        </div>
+        <div>
+          <Color handleClick = {props.update} color= "green"/> 
+        </div>  
       </div>
-    );
-}
+    ); 
+}; 
 
 export default Menu;
